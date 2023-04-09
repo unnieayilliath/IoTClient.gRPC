5da524de613d4a674cbbbbf50eda4e04207f9a38
Unnie Ayilliath <unnikannan.ayilliath@opcw.org>
2023-04-09 16:31:06 +0200

    Renamed gatewayservice

--- IoTClient.gRPC/Program.cs
+++ IoTClient.gRPC/Program.cs
@@ -10,12 +10,12 @@ httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.Danger
 var httpClient = new HttpClient(httpHandler);
 // The port number must match the port of the gRPC server.
 using var channel = GrpcChannel.ForAddress("https://localhost:5001", new GrpcChannelOptions { HttpClient = httpClient });
-var client = new IoTMessage.IoTMessageClient(channel);
+var client = new EdgeGateway.EdgeGatewayClient(channel);
 
 for (int i = 0; i < 10; i++)
 {
     var data= DataGenerator.GenerateData(1);
-    var request = new EdgeRequest { Data = data, SendTime = DateTime.UtcNow.ToTimestamp() };
+    var request = new IoTMessage { Data = data, SendTime = DateTime.UtcNow.ToTimestamp() };
     var reply = await client.SendAsync(request);
     TimeSpan ts = reply.ReceivedTime.ToDateTime() - request.SendTime.ToDateTime();
     Console.WriteLine("Latency : " + ts.Microseconds);
